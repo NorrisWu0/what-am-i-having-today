64bc038f17ee0e55f9e2ff1cb35f9ce97ab433f9
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2021-09-05 19:17:34 +1000

    Detached `<Logo/>` from `<App/>`;

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import logo from './logo.png';
+import { Logo } from './components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo />
         <p>So... What am I having today?</p>
       </header>
     </div>
