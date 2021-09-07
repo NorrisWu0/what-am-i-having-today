525a04aad0625d17d3e10e12e5e1041b8581946c
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2021-09-07 15:32:22 +1000

    Added `<Background/>`component  to `<Weather/>` section component;

---
+++ src/App/sections/LandingSection/components/Weather/components/index.js
@@ -0,0 +1,5 @@
+import Background from './Background/Background';
+// import ForecastItem from './ForecastItem/ForecastItem';
+
+// eslint-disable-next-line import/prefer-default-export
+export { Background };
