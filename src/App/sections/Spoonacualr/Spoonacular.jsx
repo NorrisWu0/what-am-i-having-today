d6692bb68ec1437f7736c7c961e09b5bb9a4edf4
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2021-10-05 19:44:40 +1100

    Created `<App/Spoonacular>` section;

---
+++ src/App/sections/Spoonacualr/Spoonacular.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Spoonacular.css';
+
+export default function Spoonacular() {
+  return (
+    <div className="spoonacular">
+      <h2>Oops, still talking to Spoonacular API, totally not slacking off.</h2>
+    </div>
+  );
+}
