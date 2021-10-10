7d6c9621463a70c5dc7dfcaea4d457ee716925f2
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2021-10-10 17:04:52 +1100

    Fixed an import reference issue in `<ReduxFun/User>`

--- src/App/sections/ReduxFun/components/User/User.jsx
+++ src/App/sections/ReduxFun/components/User/User.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { changeUsername, updateStatus } from './actions/user.action';
+import { changeUsername, updateStatus } from './user.action';
 
 import './User.css';
 
