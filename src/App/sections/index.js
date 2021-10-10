df2c191aba4e448df276802f7a2479514815ba39
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2021-10-10 17:43:43 +1100

    Added `<ReduxFun>` into `<App>`

--- src/App/sections/index.js
+++ src/App/sections/index.js
@@ -1,2 +1,3 @@
 export { default as LandingSection } from './LandingSection/LandingSection';
 export { default as Spoonacular } from './Spoonacualr/Spoonacular';
+export { default as ReduxFun } from './ReduxFun/ReduxFun';
